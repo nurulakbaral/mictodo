2bc61e96d0b4f8906534f0f43bed5f1e714835cd
nurulakbaral <nurulakbaral@gmail.com>
2022-02-13 08:47:43 +0700

    feat: add task layout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -43,37 +43,49 @@ export default function Dashboard() {
       <Head>
         <title>Mictodo - Powerfull Todo List</title>
       </Head>
-      <main className='pt-32'>
-        {/* <div>
-          <h1>
-            <Link href='/'>Home</Link>
-          </h1>
-        </div> */}
+      <main className='pt-12'>
         <div>
           <h1 className='text-4xl font-poppins text-center'>Hallo</h1>
           <h2 className='text-base font-poppins text-center'>{authorizedUser?.email}</h2>
         </div>
-        <form onSubmit={onSubmit(handleSubmit)}>
-          <div
-            onFocus={handlePlaceholderFocus}
-            onBlur={handlePlaceholderBlur}
-            className='mt-40 w-5/6 mx-auto h-12 flex items-center relative'
-          >
-            {placeholder && (
-              <div className='flex items-center absolute left-4 pointer-events-none'>
-                <PlusIcon className='w-6 h-6 mr-4 text-gray-500' />
-                <h6 className='text-gray-700 text-base'>Add a task</h6>
-              </div>
-            )}
-            <Input
-              autoComplete='off'
-              className='font-poppins'
-              focusBorderColor='twGray.400'
-              size='lg'
-              {...register('task')}
-            />
-          </div>
-        </form>
+        <div className='pt-12 pb-36'>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
+            <div key={item} className='w-5/6 mx-auto h-12 flex items-center relative mb-2'>
+              <Input
+                colorScheme='white'
+                autoComplete='off'
+                className='font-poppins'
+                focusBorderColor='twGray.400'
+                size='lg'
+                defaultValue={`My Task ${item}`}
+              />
+            </div>
+          ))}
+        </div>
+        <div className='bg-white fixed bottom-0 right-0 left-0 pt-6 pb-12 border-t-2 border-gray-100 z-10'>
+          <form className='//bg-green-400 max-w-xl mx-auto' onSubmit={onSubmit(handleSubmit)}>
+            <div
+              onFocus={handlePlaceholderFocus}
+              onBlur={handlePlaceholderBlur}
+              className='w-5/6 mx-auto h-12 flex items-center relative'
+            >
+              {placeholder && (
+                <div className='flex items-center absolute left-4 pointer-events-none'>
+                  <PlusIcon className='w-6 h-6 mr-4 text-gray-500' />
+                  <h6 className='text-gray-700 text-base'>Add a task</h6>
+                </div>
+              )}
+              <Input
+                colorScheme='white'
+                autoComplete='off'
+                className='font-poppins'
+                focusBorderColor='twGray.400'
+                size='lg'
+                {...register('task')}
+              />
+            </div>
+          </form>
+        </div>
       </main>
     </>
   )
